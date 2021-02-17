27e01acc85d2ef90e1f21bfbf85cf04550657786
Daniel Tkach <danieltkach@gmail.com>
2021-02-17 16:12:01 -0300

    2. About Us Section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import AboutUs from './pages/AboutUs';
+
 function App() {
 	return (
 		<div className="App">
 			<h1>React Portfolio</h1>
+			<AboutUs />
 		</div>
 	);
 }
---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import home1 from '../img/home1.png';
+
+const AboutSection = () => {
+	return (
+		<div>
+			<div className="description">
+				<div className="title">
+					<div className="hide"><h2>We work to make</h2></div>
+					<div className="hide"><h2>your <span>dreams</span> come</h2></div>
+					<div className="hide"><h2>true</h2></div>
+				</div>
+				<p>Contact us for photography or videography ideas. We have professionals with amazing skills.</p>
+				<button>Contact Us</button>
+			</div>
+			<div><img src={home1} alt="guy singing"/></div>
+		</div>
+	)
+}
+
+export default AboutSection;
