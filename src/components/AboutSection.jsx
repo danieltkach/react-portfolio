c7ebf872e3bb267abb2c4b6d13a68e1531e764a3
Daniel Tkach <danieltkach@gmail.com>
2021-02-18 10:44:00 -0300

    5. Intro to styled components

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,21 +1,68 @@
 import React from 'react';
 import home1 from '../img/home1.png';
+import styled from 'styled-components';
 
 const AboutSection = () => {
-	return (
-		<div>
-			<div className="description">
-				<div className="title">
-					<div className="hide"><h2>We work to make</h2></div>
-					<div className="hide"><h2>your <span>dreams</span> come</h2></div>
-					<div className="hide"><h2>true</h2></div>
-				</div>
-				<p>Contact us for photography or videography ideas. We have professionals with amazing skills.</p>
-				<button>Contact Us</button>
-			</div>
-			<div><img src={home1} alt="guy singing"/></div>
-		</div>
-	)
-}
+  return (
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>We work to make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>true</h2>
+          </Hide>
+        </div>
+        <p>
+          Contact us for photography or videography ideas. We have professionals
+          with amazing skills.
+        </p>
+        <button>Contact Us</button>
+      </Description>
+      <Image>
+        <img src={home1} alt="guy singing" />
+      </Image>
+    </About>
+  );
+};
+
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  z-index: 2;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+export const Image = styled.div`
+  z-index: 2;
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
